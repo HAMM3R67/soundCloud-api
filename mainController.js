49897ba222b8776d0040817b16e87c68adcce75a
HAMM3R67 <tcschauer@gmail.com>
2015-08-20 19:19:09 -0600

    created a scope getUser to call the getUser function from the service and console.log the informaion

--- mainController.js
+++ mainController.js
@@ -1,5 +1,9 @@
 var app = angular.module('sounder');
 
 app.controller('mainController', function($scope, soundService){
-	
+	$scope.getUser = function(){
+		soundService.getUser('Yahtzel').then(function(data){
+			console.log(data.data)
+		})
+	}
 })
