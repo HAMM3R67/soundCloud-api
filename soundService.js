39034b329660f7d81d1e070cdd7db9d78cc9f80c
HAMM3R67 <tcschauer@gmail.com>
2015-08-20 19:20:44 -0600

    adjusted service syntax

--- soundService.js
+++ soundService.js
@@ -1,10 +1,10 @@
 var app = angular.module('sounder')
 
 app.service('soundService', function($http){
-	var url = 'http://api.soundcloud.com/users/' + username + '/tracks.json?client_id=bda4ada8694db06efcac9cf97b872b3e'
+	var url = 'http://api.soundcloud.com/users/'
 	
 	this.getUser = function(username){
-		return $http.get(url)
+		return $http.get(url + username + '/tracks.json?client_id=bda4ada8694db06efcac9cf97b872b3e')
 	}
 	
 })
